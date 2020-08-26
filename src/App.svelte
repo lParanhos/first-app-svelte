d2e7ffa474b47fe40fedb3c9e081adedcb362f71
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-26 08:12:07 -0300

    Svelte app

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Comp1 from "./Compo1.svelte";
+  import Comp2 from "./Compo2.svelte";
+</script>
+
+<Comp1 />
+<Comp2 />
---
+++ src/Compo1.svelte
@@ -0,0 +1,32 @@
+<h1>Hello i'am Comp1</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus saepe
+  consequatur excepturi ea voluptates facilis porro rerum fugit? Eaque doloribus
+  perferendis ullam, ipsam aliquid voluptatibus amet at accusamus molestias
+  itaque?
+</p>
+
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus saepe
+  consequatur excepturi ea voluptates facilis porro rerum fugit? Eaque doloribus
+  perferendis ullam, ipsam aliquid voluptatibus amet at accusamus molestias
+  itaque?
+</p>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus saepe
+  consequatur excepturi ea voluptates facilis porro rerum fugit? Eaque doloribus
+  perferendis ullam, ipsam aliquid voluptatibus amet at accusamus molestias
+  itaque?
+</p>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus saepe
+  consequatur excepturi ea voluptates facilis porro rerum fugit? Eaque doloribus
+  perferendis ullam, ipsam aliquid voluptatibus amet at accusamus molestias
+  itaque?
+</p>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus saepe
+  consequatur excepturi ea voluptates facilis porro rerum fugit? Eaque doloribus
+  perferendis ullam, ipsam aliquid voluptatibus amet at accusamus molestias
+  itaque?
+</p>
