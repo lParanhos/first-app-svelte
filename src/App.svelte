006a4db9df30db43c9ad129e2f59d156ddfffad0
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-27 08:15:25 -0300

    DOM Events

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,5 @@
 <script>
-  import { each } from "svelte/internal";
-  import Card from "./Card.svelte";
-  import Compo1 from "./Compo1.svelte";
-  import Compo2 from "./Compo2.svelte";
-
-  const data = [
-    {
-      title: "First Card",
-      cover: "https://static.toiimg.com/photo/72975551.cms",
-    },
-    {
-      title: "Second Card",
-      menbersOnly: true,
-      cover:
-        "https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg",
-    },
-    {
-      title: "Third Card",
-      cover: "https://static.toiimg.com/photo/72975551.cms",
-    },
-    {
-      title: "Fourth Card",
-      cover:
-        "https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg",
-      menbersOnly: true,
-    },
-    {
-      title: "Fifth Card",
-      cover: "https://static.toiimg.com/photo/72975551.cms",
-    },
-    {
-      title: "Sixth Card",
-      menbersOnly: true,
-      cover:
-        "https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg",
-    },
-  ];
-
-  const [card1, card2] = data;
+  import List from "./List.svelte";
 </script>
 
-<div class="container mx-auto mt-4">
-  <div class="grid grid-cols-2 gap-4">
-    {#each data as item}
-      {@debug item}
-      <Card {...item} />
-    {/each}
-  </div>
-</div>
+<List />
--- src/Card.svelte
+++ src/Card.svelte
@@ -3,38 +3,45 @@
   export let cover =
     "https://i.pinimg.com/originals/ca/76/0b/ca760b70976b52578da88e06973af542.jpg";
   export let menbersOnly = false;
+  export let id = 0;
+
+  const clickHandler = () => console.log(id);
 </script>
 
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="w-full" src={cover} alt="Sunset in the mountains" />
-  <div class="px-6 py-4">
-    {#if menbersOnly}
-      <p>Menbers Only</p>
-    {:else}
-      <p>&nbsp;</p>
-    {/if}
-    <div class="font-bold text-xl mb-2">{title}</div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-      quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-      nihil.
-    </p>
+<a
+  href="https://leandroparanhos.netlify.app/"
+  on:click|preventDefault={clickHandler}>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" src={cover} alt="Sunset in the mountains" />
+    <div class="px-6 py-4">
+      {#if menbersOnly}
+        <p>Menbers Only</p>
+      {:else}
+        <p>&nbsp;</p>
+      {/if}
+      <div class="font-bold text-xl mb-2">{title}</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+        nihil.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
+        font-semibold text-gray-700 mr-2 mb-2">
+        #photography
+      </span>
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
+        font-semibold text-gray-700 mr-2 mb-2">
+        #travel
+      </span>
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
+        font-semibold text-gray-700 mr-2 mb-2">
+        #winter
+      </span>
+    </div>
   </div>
-  <div class="px-6 pt-4 pb-2">
-    <span
-      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
-      font-semibold text-gray-700 mr-2 mb-2">
-      #photography
-    </span>
-    <span
-      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
-      font-semibold text-gray-700 mr-2 mb-2">
-      #travel
-    </span>
-    <span
-      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
-      font-semibold text-gray-700 mr-2 mb-2">
-      #winter
-    </span>
-  </div>
-</div>
+</a>
