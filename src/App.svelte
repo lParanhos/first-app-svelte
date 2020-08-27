4e1d050e180c67ef6f5747add897f78a2ce779dc
lParanhos <leandro.paranhos@dextra-sw.com>
2020-08-26 23:00:14 -0300

    pass props to children component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
 <script>
-  import Comp1 from "./Compo1.svelte";
-  import Comp2 from "./Compo2.svelte";
+  import Card from "./Card.svelte";
+  import Compo1 from "./Compo1.svelte";
+  import Compo2 from "./Compo2.svelte";
+
+  const data = [
+    {
+      title: "Meu titulo 1",
+      cover: "https://static.toiimg.com/photo/72975551.cms",
+    },
+    {
+      title: "Meu titulo 2",
+      cover:
+        "https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg",
+    },
+  ];
+
+  const [card1, card2] = data;
 </script>
 
-<Comp1 />
-<Comp2 />
+<div class="container mx-auto mt-4">
+  <div class="grid grid-cols-2 gap-4">
+    <Card {...card1} />
+    <Card {...card2} />
+    <Card {...card1} />
+    <Card {...card2} />
+    <Card {...card1} />
+    <Card {...card2} />
+    <Card {...card1} />
+    <Card {...card2} />
+  </div>
+</div>
---
+++ src/Card.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let title = "My default title";
+  export let cover =
+    "https://i.pinimg.com/originals/ca/76/0b/ca760b70976b52578da88e06973af542.jpg";
+</script>
+
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src={cover} alt="Sunset in the mountains" />
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">{title}</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+      quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+      nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span
+      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
+      font-semibold text-gray-700 mr-2 mb-2">
+      #photography
+    </span>
+    <span
+      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
+      font-semibold text-gray-700 mr-2 mb-2">
+      #travel
+    </span>
+    <span
+      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
+      font-semibold text-gray-700 mr-2 mb-2">
+      #winter
+    </span>
+  </div>
+</div>
