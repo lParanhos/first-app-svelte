a775f981c90564ca67cc37139462d2e625db925a
lParanhos <leandro.paranhos@dextra-sw.com>
2020-09-03 09:24:42 -0300

    add jest dom and write test to movie container

--- src/features/Movies/Movies.spec.js
+++ src/features/Movies/Movies.spec.js
@@ -1,5 +1,12 @@
+import { render } from "@testing-library/svelte";
+import "@testing-library/jest-dom/extend-expect";
+
+import Movies from "../Movies/Movies.svelte";
+
 describe('Movies', () => {
     it('mounts MovieSearch and MovieList', () => {
-        expect(true).toBe(true);
-    })
-})
+       const { getByTestId } = render(Movies);
+       expect(getByTestId("movie-search")).toBeInTheDocument();
+       expect(getByTestId("movie-list")).toBeInTheDocument();
+    });
+});
