59621ff4152b362400dfde10166d830adad94ab1
lParanhos <leandro.paranhos@dextra-sw.com>
2020-09-03 22:43:16 -0300

    add and config tailwindcss and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enable: process.env.NODE_ENV === 'production',
+    content: [
+      './public/index.html',
+      './src/*.svelte'
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/ui")],
+}
